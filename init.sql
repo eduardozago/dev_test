28a10a0b512258ff21d6142b9bce3b57888cfda8
Eduardo Zago <zagoe22@gmail.com>
2024-11-12 22:03:13 -0300

    feat: add users and posts tables to database

--- init.sql
+++ init.sql
@@ -1,5 +1,17 @@
 USE test_db;
 
---TODO Crie a tabela de user;
+--OK Crie a tabela de user;
+CREATE TABLE users (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+);
 
---TODO Crie a tabela de posts;
+--OK Crie a tabela de posts;
+CREATE TABLE posts (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(100) NOT NULL,
+    description VARCHAR(100) NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
