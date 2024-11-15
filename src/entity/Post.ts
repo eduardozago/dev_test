da61095ecd195c8da50b60430fa2c13696990c6d
Eduardo Zago <zagoe22@gmail.com>
2024-11-15 20:02:38 -0300

    refactor: refactor user and posts entities

--- src/entity/Post.ts
+++ src/entity/Post.ts
@@ -2,11 +2,23 @@ import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from "typeorm";
 import { User } from "./User";
 
 //TODO Crie a entidade de Post
+interface PostInterface {
+    id?: number
+    title: string
+    description: string
+    userId: User
+}
 
 @Entity()
 export class Post {
+    constructor(data: PostInterface) {
+        this.title = data.title
+        this.description = data.description
+        this.userId = data.userId
+    }
+
     @PrimaryGeneratedColumn()
-    id: number
+    id?: number
 
     @Column()
     title: string
--- src/entity/User.ts
+++ src/entity/User.ts
@@ -2,11 +2,24 @@ import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from "typeorm";
 import { Post } from "./Post";
 
 //TODO Crie a entidade de User
+interface UserInterface {
+    id?: number
+    firstName: string
+    lastName: string
+    email: string
+    posts?: Post[]
+}
 
 @Entity()
-export class User {
+export class User{
+    constructor(data: UserInterface) {
+        this.firstName = data.firstName
+        this.lastName = data.lastName
+        this.email = data.email
+    } 
+
     @PrimaryGeneratedColumn()
-    id: number
+    id?: number
 
     @Column()
     firstName: string
@@ -17,6 +30,6 @@ export class User {
     @Column()
     email: string
 
-    @OneToMany(() => Post, (post) => post.user)
-    posts: Post[]
+    @OneToMany(() => Post, (post) => post.userId)
+    posts?: Post[]
 }
